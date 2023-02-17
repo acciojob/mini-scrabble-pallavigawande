884607757ea63d6e9fddd5d294ccb8943a5867cf
pallavigawande <95491010+pallavigawande@users.noreply.github.com>
2023-02-17 19:30:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+let evaluatedText = document.getElementById("evaluatedText");
+      let letterCount = document.getElementById("letterCount");
+
+      evaluatedText.addEventListener("input", lettertext);
+
+      function lettertext() {
+        let word = evaluatedText.value;
+        let letter = word.length;
+        letterCount.innerText = letter;
+      }
